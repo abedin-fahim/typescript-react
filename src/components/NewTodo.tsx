d5e2a05be01dd3b4f199455c68919d6db43b16ec
Joynal Abedin <cfcfahim26@gmail.com>
2023-06-06 21:44:09 +0600

    form submission, useRef, function props, and managing states

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,33 @@
+import { FC, useRef } from 'react';
+
+const NewTodo: FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // const enteredText = todoTextRef.current?.value;
+    const enteredText = todoTextRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // Throw an error;
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor='title'>Todo Text</label>
+      <input
+        ref={todoTextRef}
+        type='text'
+        id='title'
+      />
+      <button type='submit'>Add</button>
+    </form>
+  );
+};
+
+export default NewTodo;
